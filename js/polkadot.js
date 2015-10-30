9d0d122006338b4f82c05f893582bbe23f74699b
Jeffery Thurston <jeffery.m.thurston@saic.com>
2015-10-30 13:49:52 -0400

    Added the settings capability, added the spectrum library for color picking

--- js/polkadot.js
+++ js/polkadot.js
@@ -1,6 +1,7 @@
 var dotSize = 50; //in pixels(px) e.g. 10px
 var dotMargin = 15; //distance between each dot
 var offSet = true;
+var offset = true;
 var shape = "circle"; //circle, square, diamond, more to Chrome
 var dotQuantity = getScreenHeight(); //number of dotSize
 var dotQuantityW = getScreenWidth();
@@ -8,13 +9,14 @@ var borderWidth = 2;
 var borderColor = "white";
 var dotColor = "hotpink"; //user standard colors or hex numbers
 var bg = "black"; //user standard colors or hex numbers
-var offset = true;
 var shadow = false;
 
 $("body").css("background-color",bg);
 
 drawDots();
+
 function drawDots(){
+
   for (x = 0; x < dotQuantityW; x++){
     $("#sheet").append("<div id='vert" + x +"'></div>");
     for (i = 0; i < dotQuantity; i++){
@@ -22,14 +24,15 @@ function drawDots(){
     };
 
     if (offset == true){
+      console.log("changing offset to " + offSet)
       if (offSet == true && x % 2){
-        $("#vert"+x).css("top",calcVertOffset());
+        $("#vert"+x).css("top",calcVertOffSet());
       }else{
         $("#vert"+x).css("top","0px");
       }
     }
 
-    var nextColumn = (dotMargin + dotSize + dotMargin) * x;
+    var nextColumn = (dotSize + borderWidth + borderWidth + dotMargin) * x;
     $("#vert"+x).css("position","absolute");
     $("#vert"+x).css("left",nextColumn+"px");
   }
@@ -69,13 +72,86 @@ function getScreenHeight(){
 
 function getScreenWidth(){
   var sw = $(window).width();
-  var dotsHorizontal = sw / (dotSize + (dotMargin * 2));
+  var dotsHorizontal = sw / (dotSize + dotMargin);
   return dotsHorizontal;
 }
 
-function calcVertOffset(){
+function calcVertOffSet(){
   var vo = (dotSize + dotMargin)/2;
   return -vo + "px";
 }
 
-$(window).resize(function(){location.reload();});
+//$(window).resize(function(){location.reload();});
+$(window).resize(function(){
+  $("#sheet").empty();
+  console.log("redrawing!");
+  drawDots();
+})
+
+function closeMenu(){
+  $( "#menuContainer" ).animate({
+      left: "-=170"
+    }, 500 );
+  $("#menuTabContent").text("🔼Settings");
+  //$("#menu").css("top","-150px");//
+  //$("#menuTab").css("top","0px");//
+  $('#menuTab').attr('onclick', '');
+  $('#menuTab').attr('onclick','openMenu()');
+}
+
+function openMenu(){
+  $( "#menuContainer" ).animate({
+      left: "+=170"
+    }, 500 );
+  $("#menuTabContent").text("🔽Close");
+  $("#menu").css("left","0px");
+  $('#menuTab').attr('onclick', '');
+  $('#menuTab').attr('onclick','closeMenu()');
+}
+
+function changeShape(theShape){
+  if(theShape == "square"){
+    $(".shape").css("transform", "rotate(0deg)");
+    $(".shape").css("border-radius","0px"); //set radius of polks dot
+  }
+
+  if(theShape == "circle"){
+    $(".shape").css("border-radius",dotSize + "px"); //set radius of polks dot
+  }
+
+  if(theShape == "diamond"){
+    $(".shape").css("border-radius","0px"); //set radius of polks dot
+    $(".shape").css("transform", "rotate(45deg)");
+    $(".shape").css("margin",((dotMargin) + "px")); //set distance around the dot
+  }
+  shape = theShape;
+}
+
+function changeBorderSize(theBorderSize){
+  console.log("Changing border size to: " + theBorderSize);
+  $(".shape").css("border-width",theBorderSize); //set color of polka dot
+  borderWidth = theBorderSize;  
+}
+
+function changeOffSet(theOffSet){
+  console.log("Changing the offset to: " + theOffSet);
+  if (theOffSet == true){
+    offSet = true;
+    offSet = true;
+    drawDots();
+  } else {
+    offSet = false;
+    offSet = false;
+    drawDots();
+  }
+}
+
+function changeSize(theSize){
+  console.log("Changing size to: " + theSize);
+  $(".shape").css("width",theSize + "px"); //set width of polka dot
+  $(".shape").css("height",theSize + "px"); //set heigh of polka dot
+  if(shape == "circle"){
+    $(".shape").css("border-radius",theSize + "px"); //set heigh of polka dot
+  }
+  dotSize = theSize;
+}
